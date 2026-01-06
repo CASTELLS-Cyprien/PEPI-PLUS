11a2b120d6b84d631e70e58b2c59a323e4cc788f
DOLPIXS <castellscyprien@gmail.com>
2026-01-06 16:05:09 +0100

    update

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,8 @@ Encore
      */
     .addEntry('app', './assets/app.js')
 
+    .addStyleEntry('app', './assets/styles/app.css')
+
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
 
